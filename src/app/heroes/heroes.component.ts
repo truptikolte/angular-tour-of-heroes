b4b6624b072f7b3c6c00dac7dd0b911b625440d9
truptikolte <62907485+truptikolte@users.noreply.github.com>
2021-02-10 18:37:12 +0000

    Display Name of the Hero

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+export class HeroesComponent implements OnInit {
+  hero = 'Nitin';
+  ngOnInit(): void {
+  }
+
+}
