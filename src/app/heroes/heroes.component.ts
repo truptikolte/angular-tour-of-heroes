1e327b0d7375c3451337a13da2e70ac51662291c
truptikolte <62907485+truptikolte@users.noreply.github.com>
2021-02-12 20:51:52 +0000

    Display list of Heroes and on click show details

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
 @Component({
   selector: 'app-heroes',
@@ -7,6 +8,7 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
   heroes = HEROES;
+  selectedHero?: Hero;
 
   /*hero: Hero = {
     id : 1,
@@ -17,6 +19,10 @@ export class HeroesComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
