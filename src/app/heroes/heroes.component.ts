381262f1b25a8ba546e563ef3a83e53c7f0154d0
truptikolte <62907485+truptikolte@users.noreply.github.com>
2021-02-12 11:13:14 +0000

    Display List of Heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
+  heroes = HEROES;
+
+  /*hero: Hero = {
     id : 1,
     name : 'Nitin'
-  };
+  };*/
 
   /*hero = 'Nitin'*/
   constructor() { }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    {id:11, name:'Dr Nice'},
+    {id:13, name:'Soham'},
+    {id:14, name:'Nitin'},
+    {id:15, name:'Milind'},
+    {id:16, name:'Dr Rahul'},
+    {id:17, name:'Kanta'},
+    {id:18, name:'shiva'},
+    {id:19, name:'Baba'},
+    {id:20, name:'ishan'}
+];
