4cbf062e791ed3a70304c8465e7d06f7bdb3bd75
truptikolte <tnkoltegmail.com>
2021-07-29 10:41:47 +0100

    Delete Hero Feature Complete

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -51,6 +51,14 @@ export class HeroService {
       catchError(this.handleError<Hero>('addHero'))
     );
   }
+
+  deleteHero(id: number): Observable<Hero>{
+    const url = `${this.heroesUrl}/${id}`;
+    return this,this.http.delete<Hero>(url, this.httpOptions).pipe(
+      tap(_ =>this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHEro'))
+    );
+  }
   
   private handleError<T>(operattion = 'operation', result?:T){
     return (error:any):Observable<T> => {
